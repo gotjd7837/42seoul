45639862b7ce6362bcaa9168ef8d3a008cffef8b
Haeseong Kang <haekang@c2r9s4.42seoul.kr>
2023-04-06 21:57:20 +0900

    last

--- 42cursus/libft/libft.h
+++ 42cursus/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: haekang <haekang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 00:44:39 by haekang           #+#    #+#             */
-/*   Updated: 2023/04/06 19:56:59 by haekang          ###   ########.fr       */
+/*   Updated: 2023/04/06 21:56:59 by haekang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,5 +63,6 @@ void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list  *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
