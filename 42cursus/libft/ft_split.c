782c374107c72427250a0b985d250c2322c5074e
Haeseong Kang <haekang@c2r3s1.42seoul.kr>
2023-04-09 14:55:01 +0900

    last

--- 42cursus/libft/ft_split.c
+++ 42cursus/libft/ft_split.c
@@ -6,75 +6,78 @@
 /*   By: haekang <haekang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 02:25:24 by haekang           #+#    #+#             */
-/*   Updated: 2023/04/07 17:48:04 by haekang          ###   ########.fr       */
+/*   Updated: 2023/04/09 14:21:52 by haekang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-
-static char *result_malloc(char *str, char c, char **result, int idx)
+static char	*result_malloc(char *str, char c, char **result, int idx)
 {
-    int len = 0;
+	int	len;
 
-    while (str[len] && str[len] != c)
-        len++;
-    result[idx] = (char *)malloc(sizeof(char) * (len + 1));
-    if (result[idx] == NULL)
-    {
-        while (--idx >= 0)
+	len = 0;
+	while (str[len] && str[len] != c)
+		len++;
+	result[idx] = (char *)malloc(sizeof(char) * (len + 1));
+	if (result[idx] == NULL)
+	{
+		while (--idx >= 0)
 			free(result[idx]);
-        free(result);
-        return (NULL);
-    }
-    ft_strlcpy(result[idx], str, len + 1);
-    return (result[idx]);
+		free(result);
+		return (NULL);
+	}
+	ft_strlcpy(result[idx], str, len + 1);
+	return (result[idx]);
 }
 
-static char **result_double_malloc(char *str, char c)
+static char	**result_double_malloc(char *str, char c)
 {
-    int cnt = 0;
-    char **result;
+	int		cnt;
+	char	**result;
 
-    while (*str)
-    {
-        if (*str != c)
-        {
-            cnt++;
-            while (*str && *str != c)
-                str++;
-        }
-        else
-            str++;
-    }
-    result = (char **)malloc(sizeof(char *) * (cnt + 1));
-    if (!result)
-        return (NULL);
-    return (result);
-}
-
-char **ft_split(char const *s, char c)
-{
-    char *arr = (char *)s;
-    char **result;
-    int i = 0;
-
-    if (!s)
-        return (NULL);
-    result = result_double_malloc(arr, c);
-    if (result == NULL)
-        return (NULL);
-    while (*arr)
-    {
-        if (*arr != c)
-        {
-            if (result_malloc(arr, c, result, i++) == NULL)
-                return (NULL);
-            while (*arr && *arr != c)
-				arr++;
+	cnt = 0;
+	while (*str)
+	{
+		if (*str != c)
+		{
+			cnt++;
+			while (*str && *str != c)
+				str++;
 		}
 		else
-			arr++;
+			str++;
+	}
+	result = (char **)malloc(sizeof(char *) * (cnt + 1));
+	if (result == NULL)
+		return (NULL);
+	return (result);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	*str;
+	char	**result;
+	int		i;
+
+	str = (char *)s;
+	i = 0;
+	if (s == NULL)
+		return (NULL);
+	result = result_double_malloc(str, c);
+	if (result == NULL)
+		return (NULL);
+	while (*str)
+	{
+		if (*str != c)
+		{
+			if (result_malloc(str, c, result, i++) == NULL)
+				return (NULL);
+			while (*str && *str != c)
+				str++;
+		}
+		else
+			str++;
 	}
 	result[i] = NULL;
 	return (result);
