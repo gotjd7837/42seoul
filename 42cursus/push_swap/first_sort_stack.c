79c4cf29cb82587088a55518e27eaa2242fd59aa
Haeseong Kang <gotjd7837@gmail.com>
2023-07-11 13:06:01 +0900

    push_swap

--- 42cursus/push_swap/first_sort_stack.c
+++ 42cursus/push_swap/first_sort_stack.c
@@ -6,20 +6,20 @@
 /*   By: haekang <haekang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 16:43:34 by haekang           #+#    #+#             */
-/*   Updated: 2023/06/30 20:06:25 by haekang          ###   ########.fr       */
+/*   Updated: 2023/07/06 16:39:00 by haekang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	pb_except_three(t_stack *stack_a, t_stack *stack_b)
+static void	pb_except_five(t_stack *stack_a, t_stack *stack_b)
 {
 	int	i;
 
 	i = 0;
-	if (stack_a->size <= 3)
+	if (stack_a->size <= 5)
 		return ;
-	while (stack_a->size > 3)
+	while (stack_a->size > 5)
 		pb(stack_a, stack_b);
 }
 
@@ -52,5 +52,5 @@ void	first_sort_stack(t_stack *a, t_stack *b, int *sorted_data)
 	pivot[1] = sorted_data[(max_index_arr * 2) / 3];
 	a_to_b(a, b, pivot[0]);
 	a_to_b(a, b, pivot[1]);
-	pb_except_three(a, b);
+	pb_except_five(a, b);
 }
