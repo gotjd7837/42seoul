3dd98dffeb98d12069af4cd0709d1ee72d82d363
Haeseong Kang <haekang@c2r9s4.42seoul.kr>
2023-12-16 16:33:09 +0900

    cub3d

--- 42cursus/cub3D/includes/cub3d.h
+++ 42cursus/cub3D/includes/cub3d.h
@@ -1,30 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: haekang <haekang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/22 08:10:25 by haekang           #+#    #+#             */
-/*   Updated: 2023/12/16 16:07:52 by haekang          ###   ########.fr       */
+/*   Created: 2023/12/16 15:53:12 by haekang           #+#    #+#             */
+/*   Updated: 2023/12/16 16:23:19 by haekang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "so_long.h"
+#ifndef CUB3D_H
+# define CUB3D_H
 
-int	main(int ac, char *av[])
+# include "mlx/mlx.h"
+
+typedef struct s_info
 {
-	t_info	info;
 
-	if (ac != 2)
-	{
-		ft_printf("Error\n>하나의 맵 파일을 입력하세요\n");
-		return (1);
-	}
-	set_info(&info, av[1]);
-	draw(&info);
-	mlx_hook(info.win, PRESS_RED_BUTTON, 0, press_red_button, &info);
-	mlx_key_hook(info.win, key_press, &info);
-	mlx_loop(info.mlx);
-	return (0);
-}
+}				t_info;
+
+#endif
--- 42cursus/cub3D/sources/cub_main/cub_main.c
+++ 42cursus/cub3D/sources/cub_main/cub_main.c
@@ -1,30 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   cub_main.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: haekang <haekang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/22 08:10:25 by haekang           #+#    #+#             */
-/*   Updated: 2023/12/16 16:07:52 by haekang          ###   ########.fr       */
+/*   Created: 2023/12/16 16:16:54 by haekang           #+#    #+#             */
+/*   Updated: 2023/12/16 16:23:28 by haekang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "so_long.h"
+#include "../../includes/cub3d.h"
 
 int	main(int ac, char *av[])
 {
 	t_info	info;
-
-	if (ac != 2)
-	{
-		ft_printf("Error\n>하나의 맵 파일을 입력하세요\n");
-		return (1);
-	}
-	set_info(&info, av[1]);
-	draw(&info);
-	mlx_hook(info.win, PRESS_RED_BUTTON, 0, press_red_button, &info);
-	mlx_key_hook(info.win, key_press, &info);
-	mlx_loop(info.mlx);
-	return (0);
+	
 }
